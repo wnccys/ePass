e3dd71dde1fe43bc69b96f105e7b0b546bed9991
wnccys <ipooo346@gmail.com>
2026-05-22 16:41:22 -0300

    feat(components): add shader background

---
+++ src/epass-web/components/ui/shader-background.tsx
@@ -0,0 +1,27 @@
+"use client"
+
+import { GrainGradient } from "@paper-design/shaders-react"
+
+export function GradientBackground() {
+  return (
+    <div className="absolute inset-0 -z-10">
+      <GrainGradient
+        style={{ height: "100%", width: "100%" }}
+        colorBack="hsl(0, 0%, 0%)"
+        softness={0.76}
+        intensity={0.45}
+        noise={0}
+        shape="corners"
+        offsetX={0}
+        offsetY={0}
+        scale={1}
+        rotation={0}
+        speed={1}
+        colors={[
+            "hsl(167, 59%, 14%)",
+            "hsl(67, 87%, 59%)",
+        ]}
+      />
+    </div>
+  )
+}
