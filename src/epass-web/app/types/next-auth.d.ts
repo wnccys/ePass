269952d55b64d268b2690693b01fb576b5638558
wnccys <ipooo346@gmail.com>
2026-05-25 19:09:22 -0300

    feat(next-auth): set extra context

---
+++ src/epass-web/app/types/next-auth.d.ts
@@ -0,0 +1,25 @@
+import NextAuth, { DefaultSession } from "next-auth";
+
+declare module "next-auth" {
+  /**
+   * Returned by `useSession`, `getServerSession` and received as a prop on the `SessionProvider` React Context
+   */
+  interface Session {
+    user: {
+      /** The user's database unique Mongoose _id. */
+      id: string;
+    } & DefaultSession["user"];
+  }
+
+  interface User {
+    id: string;
+  }
+}
+
+declare module "next-auth/jwt" {
+  /** Returned by the `jwt` callback and `getToken`, when using JWT sessions */
+  interface JWT {
+    /** The user's database unique Mongoose _id. */
+    id?: string;
+  }
+}
