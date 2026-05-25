0aa5ff2d257b2e517a42f0c84724c5df0a7059d1
wnccys <ipooo346@gmail.com>
2026-05-25 19:08:47 -0300

    feat(user-schema|mongoose): adjust User model type support

---
+++ src/epass-web/models/User.ts
@@ -0,0 +1,26 @@
+import mongoose, { Schema, Document, Model } from "mongoose";
+
+export interface IUser extends Document {
+  name: string;
+  email: string;
+  image?: string;         // Optional
+  authProvider: string;
+  authProviderId: string;
+}
+
+// Pass the interface into the Schema via <IUser>
+const UserSchema = new Schema<IUser>({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  image: { type: String },
+  authProvider: { type: String, required: true },
+  authProviderId: { type: String, required: true },
+}, {
+  timestamps: true // Automatically adds createdAt and updatedAt
+});
+
+// Pass the interface into the Model via Model<IUser> and model<IUser>
+// The logical OR (||) prevents Next.js from recompiling the model multiple times in development
+const User: Model<IUser> = mongoose.models.User || mongoose.model<IUser>("User", UserSchema);
+
+export default User;
