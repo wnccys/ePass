5e59c63b8a4660619111dbb246d0fe0c99ce6356
wnccys <ipooo346@gmail.com>
2026-05-25 19:32:00 -0300

    feat(login/logout-button): adjust button to layout

--- src/epass-web/app/login/page.tsx
+++ src/epass-web/app/login/page.tsx
@@ -1,17 +1,18 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { signIn } from "next-auth/react";
 
 export default function LoginPage() {
     return (
         <div className="flex flex-col items-center justify-center min-h-screen">
         <h1>Welcome Back</h1>
-        <button
+        <Button
             onClick={() => signIn("google", { callbackUrl: "/home" })}
-            className="px-4 py-2 bg-blue-600 text-white rounded"
+            className="px-4 py-2 rounded"
         >
             Sign in with Google
-        </button>
+        </Button>
         </div>
     );
 }
