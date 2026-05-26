ebcf084846e1247a495225ac1fb856e6eaee5ac9
wnccys <ipooo346@gmail.com>
2026-05-26 09:54:35 -0300

    ref(login/page): adjust indentation

--- src/epass-web/app/login/page.tsx
+++ src/epass-web/app/login/page.tsx
@@ -6,13 +6,13 @@ import { signIn } from "next-auth/react";
 export default function LoginPage() {
     return (
         <div className="flex flex-col items-center justify-center min-h-screen">
-        <h1>Welcome Back</h1>
-        <Button
-            onClick={() => signIn("google", { callbackUrl: "/home" })}
-            className="px-4 py-2 rounded"
-        >
-            Sign in with Google
-        </Button>
+            <h1>Welcome Back</h1>
+            <Button
+                onClick={() => signIn("google", { callbackUrl: "/home" })}
+                className="px-4 py-2 rounded"
+            >
+                Sign in with Google
+            </Button>
         </div>
     );
 }
