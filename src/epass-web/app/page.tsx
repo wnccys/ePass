08e957c1e6f5d26d7a217b6ca5147222bf5e59a3
wnccys <ipooo346@gmail.com>
2026-05-26 12:11:21 -0300

    feat(page.tsx): set login redirection

--- src/epass-web/app/page.tsx
+++ src/epass-web/app/page.tsx
@@ -3,37 +3,45 @@ import { GradientBackground } from "@/components/ui/shader-background";
 import { LiquidGlassNavbar } from "@/components/ui/liquid-glass-navbar";
 import Link from "next/link";
 import Image from 'next/image';
+import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Page() {
+    const session = await getServerSession(authOptions);
+
+    // Logged
+    if (session) redirect("/home");
+
     return (
-    <main className="relative min-h-screen h-full w-full flex items-center justify-center overflow-hidden">
-        <GradientBackground />
-        {/* Oppacity Filter */}
-        <div className="absolute inset-0 -z-10 bg-black/15" />
+        <main className="relative min-h-screen h-full w-full flex items-center justify-center overflow-hidden">
+            <GradientBackground />
+            {/* Oppacity Filter */}
+            <div className="absolute inset-0 -z-10 bg-black/15" />
 
-        <div className="fixed top-8 left-0 right-0 flex justify-center z-50">
-            <LiquidGlassNavbar>
-                <div className="flex items-center select-none cursor-pointer gap-1.5">
-                    <Image
-                        src="/favicon.png"
-                        alt="Icon"
-                        width={20}
-                        height={20}
-                        priority
-                    />
-                    <Link href="/" className="font-light text-white text-xl">ePass</Link>
-                </div>
-                <div className="flex items-center gap-6 text-sm text-zinc-300 [&_a]:cursor-default [&_a]:select-none">
-                    <a href="#" className="hover:text-white transition-colors">Clubs</a>
-                    <a href="#" className="hover:text-white transition-colors">Players</a>
-                    <a href="#" className="hover:text-white transition-colors">About</a>
-                </div>
-            </LiquidGlassNavbar>
-        </div>
+            <div className="fixed top-8 left-0 right-0 flex justify-center z-50">
+                <LiquidGlassNavbar>
+                    <div className="flex items-center select-none cursor-pointer gap-1.5">
+                        <Image
+                            src="/favicon.png"
+                            alt="Icon"
+                            width={20}
+                            height={20}
+                            priority
+                        />
+                        <Link href="/" className="font-light text-white text-xl">ePass</Link>
+                    </div>
+                    <div className="flex items-center gap-6 text-sm text-zinc-300 [&_a]:cursor-default [&_a]:select-none">
+                        <a href="#" className="hover:text-white transition-colors">Clubs</a>
+                        <a href="#" className="hover:text-white transition-colors">Players</a>
+                        <a href="#" className="hover:text-white transition-colors">About</a>
+                    </div>
+                </LiquidGlassNavbar>
+            </div>
 
-        <section className="px-6">
-            <HeroSection />
-        </section>
-    </main>
-    );
+            <section className="px-6">
+                <HeroSection />
+            </section>
+        </main>
+    )
 }
