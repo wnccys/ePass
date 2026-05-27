da39e1d6a8b12bb4014888db05c498679949ebf8
wnccys <ipooo346@gmail.com>
2026-05-27 10:40:54 -0300

    feat(route): properly type role

--- src/epass-web/app/api/auth/[...nextauth]/route.ts
+++ src/epass-web/app/api/auth/[...nextauth]/route.ts
@@ -45,7 +45,7 @@ export const authOptions: NextAuthOptions = {
         async session({ session, token }) {
             if (session.user) {
                 session.user.id = token.id as string;
-                session.user.role = token.role as string;
+                session.user.role = token.role as "player" | "club";
                 session.user.onboardingComplete = token.onboardingComplete as boolean;
             }
 
