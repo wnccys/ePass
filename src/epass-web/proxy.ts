83c14d3796b5e95d96a629a4858dd1f8badb19ea
wnccys <ipooo346@gmail.com>
2026-05-27 14:57:36 -0300

    feat(home/globals): move redirect logic to proxy / adjust input border on globals.css

--- src/epass-web/proxy.ts
+++ src/epass-web/proxy.ts
@@ -5,6 +5,7 @@ import type { NextRequest } from "next/server";
 export default async function proxy(req: NextRequest) {
     const token = await getToken({ req });
     const isAuth = !!token;
+    const isLanding = req.nextUrl.pathname.startsWith("/");
     const isLoginPage = req.nextUrl.pathname.startsWith("/login");
     const isHome = req.nextUrl.pathname.startsWith("/home");
     const isProfile = req.nextUrl.pathname.startsWith("/profile");
@@ -20,6 +21,11 @@ export default async function proxy(req: NextRequest) {
     // 2. Protected Routes: Unlogged users cannot access /home
     if ((isHome || isProfile) && !isAuth) {
         return NextResponse.redirect(new URL("/login", req.url));
+    }
+
+    // 3. Redirect from '/' to '/home' when logged
+    if (isLanding && isAuth) {
+        return NextResponse.redirect(new URL("/home", req.url));
     }
 
     return NextResponse.next();
