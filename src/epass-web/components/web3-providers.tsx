d52a449c2d28cceb4af046034f8ca429b5c709e1
wnccys <ipooo346@gmail.com>
2026-05-28 07:49:21 -0300

    feat(providers): configure foundry

--- src/epass-web/components/web3-providers.tsx
+++ src/epass-web/components/web3-providers.tsx
@@ -2,34 +2,41 @@
 
 import React, { useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { getDefaultConfig, RainbowKitAuthenticationProvider, RainbowKitProvider } from "@rainbow-me/rainbowkit";
-import { hardhat, mainnet } from "wagmi/chains";
+import {
+  getDefaultConfig,
+  RainbowKitProvider,
+} from "@rainbow-me/rainbowkit";
+import { injectedWallet } from '@rainbow-me/rainbowkit/wallets';
+import { foundry } from "wagmi/chains";
 import { WagmiProvider, http } from "wagmi";
-import { linkingAdapter } from '@/lib/siwe-adapter';
 
 export function Web3Providers({ children }: { children: React.ReactNode }) {
     // Instantiate inside the component to prevent SSR data leaks!
     const [queryClient] = useState(() => new QueryClient());
+    const foundryRpcUrl = "http://127.0.0.1:8545";
 
     const config = getDefaultConfig({
         appName: 'Football Transfer Portal',
         projectId: 'ba9cdaa4859ae0262a0ecdc00bd534f1',
-        chains: [mainnet, hardhat],
+        chains: [foundry],
+        wallets: [
+            {
+                groupName: 'Recommended',
+                wallets: [injectedWallet],
+            },
+        ],
         ssr: true, // TWEAK 2: You MUST add this for Next.js App Router
         transports: {
-            [mainnet.id]: http(),
-            [hardhat.id]: http(),
+            [foundry.id]: http(foundryRpcUrl),
         },
     });
 
     return (
         <WagmiProvider config={config}>
             <QueryClientProvider client={queryClient}>
-                <RainbowKitAuthenticationProvider adapter={linkingAdapter} status="unauthenticated">
                 <RainbowKitProvider>
                     {children}
                 </RainbowKitProvider>
-                </RainbowKitAuthenticationProvider>
             </QueryClientProvider>
         </WagmiProvider>
     );
