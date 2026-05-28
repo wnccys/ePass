de9fda7d6ee1605dd5390798127f948681ced559
RosettiBR <matheus.rosetti@outlook.com>
2026-05-28 12:40:32 -0300

    Corrigindo contratos - preenchendo endereços de carteira + status de transferencia + distribuição de tokens

--- src/smart-contracts/src/PlayerRightsMaster.sol
+++ src/smart-contracts/src/PlayerRightsMaster.sol
@@ -8,42 +8,67 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 contract PlayerRightsMaster is ERC721URIStorage, Ownable {
     uint256 private _nextTokenId;
 
-
     address public authorizedMinter;
+    mapping(address => bool) public authorizedOperators;
 
     error CallerNotAuthorized();
     error ZeroAddress();
 
-    event RightsMinted(
-        uint256 indexed tokenId,
-        address indexed recipient,
-        string tokenURI
-    );
+    event AuthorizedMinterUpdated(address indexed minter);
+    event OperatorAuthorizationUpdated(address indexed operator, bool allowed);
+    event RightsMinted(uint256 indexed tokenId, address indexed recipient, string tokenURI);
 
-    constructor(
-        address initialOwner
-    ) ERC721("Player Rights Master", "PRM") Ownable(initialOwner) {}
-
+    constructor(address initialOwner) ERC721("Player Rights Master", "PRM") Ownable(initialOwner) {}
 
     function setAuthorizedMinter(address _minter) external onlyOwner {
         if (_minter == address(0)) revert ZeroAddress();
         authorizedMinter = _minter;
+        emit AuthorizedMinterUpdated(_minter);
     }
 
-    
-    function mintRights(
-        address recipient,
-        string calldata uri
-    ) external returns (uint256) {
+    function setAuthorizedOperator(address operator, bool allowed) external onlyOwner {
+        if (operator == address(0)) revert ZeroAddress();
+        authorizedOperators[operator] = allowed;
+        emit OperatorAuthorizationUpdated(operator, allowed);
+    }
+
+    function mintRights(address recipient, string calldata uri) external returns (uint256) {
         if (msg.sender != authorizedMinter) revert CallerNotAuthorized();
+        if (recipient == address(0)) revert ZeroAddress();
 
         uint256 tokenId = ++_nextTokenId;
-
         _safeMint(recipient, tokenId);
         _setTokenURI(tokenId, uri);
 
         emit RightsMinted(tokenId, recipient, uri);
-
         return tokenId;
+    }
+
+    function _update(address to, uint256 tokenId, address auth) internal override returns (address) {
+        address from = _ownerOf(tokenId);
+
+        if (from != address(0) && to != address(0) && !authorizedOperators[msg.sender]) {
+            revert CallerNotAuthorized();
+        }
+
+        return super._update(to, tokenId, auth);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721URIStorage)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
     }
 }
--- src/smart-contracts/test/PlayerRightsMaster.t.sol
+++ src/smart-contracts/test/PlayerRightsMaster.t.sol
@@ -9,7 +9,9 @@ contract PlayerRightsMasterTest is Test {
 
     address owner = address(1);
     address minter = address(2);
-    address recipient = address(3);
+    address operator = address(3);
+    address recipient = address(4);
+    address other = address(5);
 
     function setUp() public {
         vm.prank(owner);
@@ -23,26 +25,60 @@ contract PlayerRightsMasterTest is Test {
         assertEq(master.authorizedMinter(), minter);
     }
 
-    function testNonOwnerCannotSetAuthorizedMinter() public {
-        vm.prank(minter);
-        vm.expectRevert();
-        master.setAuthorizedMinter(minter);
-    }
-
     function testAuthorizedMinterCanMintRights() public {
-        vm.prank(owner);
+        vm.startPrank(owner);
         master.setAuthorizedMinter(minter);
+        vm.stopPrank();
 
         vm.prank(minter);
         uint256 tokenId = master.mintRights(recipient, "ipfs://token-metadata");
 
         assertEq(tokenId, 1);
         assertEq(master.ownerOf(tokenId), recipient);
+        assertEq(master.tokenURI(tokenId), "ipfs://token-metadata");
     }
 
     function testNonAuthorizedMinterCannotMintRights() public {
         vm.prank(recipient);
         vm.expectRevert(PlayerRightsMaster.CallerNotAuthorized.selector);
         master.mintRights(recipient, "ipfs://token-metadata");
+    }
+
+    function testOwnerCanAuthorizeOperator() public {
+        vm.prank(owner);
+        master.setAuthorizedOperator(operator, true);
+
+        assertTrue(master.authorizedOperators(operator));
+    }
+
+    function testTransferRevertsForNonAuthorizedOperator() public {
+        vm.startPrank(owner);
+        master.setAuthorizedMinter(minter);
+        vm.stopPrank();
+
+        vm.prank(minter);
+        uint256 tokenId = master.mintRights(recipient, "ipfs://token-metadata");
+
+        vm.prank(recipient);
+        vm.expectRevert(PlayerRightsMaster.CallerNotAuthorized.selector);
+        master.transferFrom(recipient, other, tokenId);
+    }
+
+    function testAuthorizedOperatorCanTransfer() public {
+        vm.startPrank(owner);
+        master.setAuthorizedMinter(minter);
+        master.setAuthorizedOperator(operator, true);
+        vm.stopPrank();
+
+        vm.prank(minter);
+        uint256 tokenId = master.mintRights(recipient, "ipfs://token-metadata");
+
+        vm.prank(recipient);
+        master.approve(operator, tokenId);
+
+        vm.prank(operator);
+        master.transferFrom(recipient, other, tokenId);
+
+        assertEq(master.ownerOf(tokenId), other);
     }
 }
