8d91ec08dabb6f6543fc5f3886235e9f30a15640
wnccys <ipooo346@gmail.com>
2026-05-28 13:21:28 -0300

    feat(app/login): componentize public nav

--- src/epass-web/app/login/page.tsx
+++ src/epass-web/app/login/page.tsx
@@ -2,11 +2,19 @@ import { AuthComponent } from "@/components/ui/sign-up";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../api/auth/[...nextauth]/route";
 import { redirect } from "next/navigation";
+import { PublicNavbar } from "@/components/public-navbar";
 
 export default async function LoginPage() {
     const session = await getServerSession(authOptions);
     // If not logged, render signin form
-    if (!session) return <AuthComponent />
+    if (!session) {
+        return (
+            <>
+                <PublicNavbar />
+                <AuthComponent />
+            </>
+        );
+    }
 
     redirect("/home");
 }
