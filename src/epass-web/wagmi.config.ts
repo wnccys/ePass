00e0e5311802c8fef891e8978ce34f453349a500
wnccys <ipooo346@gmail.com>
2026-06-02 18:26:42 -0300

    feat(wagmi): adjust configuration file

--- src/epass-web/wagmi.config.ts
+++ src/epass-web/wagmi.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from '@wagmi/cli'
-import { react } from '@wagmi/cli/plugins'
+import { foundry, react } from '@wagmi/cli/plugins'
 
 export default defineConfig({
   out: 'src/generated.ts',
   contracts: [],
   plugins: [
+    foundry({
+      // Points to your foundry root directory relative to this frontend root
+      project: '../smart-contracts',
+
+      // Optional: If your foundry.toml uses a custom output folder instead of 'out'
+      artifacts: 'out/',
+    }),
     react()
   ],
 })
