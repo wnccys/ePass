f9f602bfe900d3d5ab1854bb6a48f7aec94ab900
wnccys <ipooo346@gmail.com>
2026-06-02 18:15:20 -0300

    lib(wagmi-cli): set package/script

---
+++ src/epass-web/wagmi.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from '@wagmi/cli'
+import { react } from '@wagmi/cli/plugins'
+
+export default defineConfig({
+  out: 'src/generated.ts',
+  contracts: [],
+  plugins: [
+    react()
+  ],
+})
