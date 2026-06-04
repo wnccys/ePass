76fbbb29d6bdf3f5aff23f8606e49ac929d25dca
wnccys <ipooo346@gmail.com>
2026-06-04 00:14:17 -0300

    chore(mongo): set database explorer

--- scripts/connect-mongo.sh
+++ scripts/connect-mongo.sh
@@ -1,1 +1,2 @@
 docker exec -it epass-mongo mongosh -u root -p secretpassword --authenticationDatabase admin epass_db
+# ConnectionString: mongodb://root:secretpassword@localhost:27017/epass_db?authSource=admin
