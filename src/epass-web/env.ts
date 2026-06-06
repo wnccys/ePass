64db6ade0a05cd9bba5176a7064befe68b8b4b25
wnccys <ipooo346@gmail.com>
2026-06-06 15:04:33 -0300

    fix/chore(sepolia/url): adjust sepolia rpc url

--- src/epass-web/env.ts
+++ src/epass-web/env.ts
@@ -72,7 +72,7 @@ export const env = createEnv({
     ADMIN_PRIVATE_KEY: process.env.ADMIN_PRIVATE_KEY,
     GROQ_API_KEY: process.env.GROQ_API_KEY,
     GROQ_MODEL: process.env.GROQ_MODEL,
-    SEPOLIA_RPC_URL: process.env.SEPOLIA_RPC,
+    SEPOLIA_RPC_URL: process.env.SEPOLIA_RPC_URL,
 
     NEXT_PUBLIC_RAINBOW_PROJECT_ID: process.env.NEXT_PUBLIC_RAINBOW_PROJECT_ID,
     NEXT_PUBLIC_APP_NETWORK: process.env.NEXT_PUBLIC_APP_NETWORK,
