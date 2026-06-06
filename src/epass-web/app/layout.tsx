6581961b1ef08f03994660a09890b04e250ff64f
wnccys <ipooo346@gmail.com>
2026-06-06 14:44:22 -0300

    fix(layout): set correct head tag location

--- src/epass-web/app/layout.tsx
+++ src/epass-web/app/layout.tsx
@@ -45,7 +45,11 @@ export default async function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <>
+        <html
+            lang="en"
+            className={cn("h-full", "antialiased", rodinProB.variable, "font-serif", merriweather.variable, notoSerifHeading.variable)}
+            suppressHydrationWarning
+        >
             <head>
                 {/* React Scan -- DevTool */}
                 {/* <script
@@ -53,21 +57,15 @@ export default async function RootLayout({
                     src="//unpkg.com/react-scan/dist/auto.global.js"
                 ></script> */}
             </head>
-                <html
-                    lang="en"
-                    className={cn("h-full", "antialiased", rodinProB.variable, "font-serif", merriweather.variable, notoSerifHeading.variable)}
-                    suppressHydrationWarning
-                >
-                    <body className="min-h-full flex flex-col selection:bg-lime-200/80">
-                        <AppProviders>
-                            <Web3Providers>
-                                <MainNavbar />
-                                {children}
-                                <Toaster />
-                            </Web3Providers>
-                        </AppProviders>
-                </body>
-            </html>
-        </>
+            <body className="min-h-full flex flex-col selection:bg-lime-200/80">
+                <AppProviders>
+                    <Web3Providers>
+                        <MainNavbar />
+                        {children}
+                        <Toaster />
+                    </Web3Providers>
+                </AppProviders>
+            </body>
+        </html>
     );
 }
